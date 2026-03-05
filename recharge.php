8e6bcacbadf0bd6c1ce1f6eb488f186d4afd88ab
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2026-03-05 11:36:36 +0100

    Create recharge.php

---
+++ recharge.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+require_once "config/database.php";
+
+if(!isset($_SESSION['user_id'])){
+header("Location: login.php");
+exit;
+}
+
+$methods = $pdo->query("SELECT * FROM payment_methods WHERE status=1 ORDER BY id ASC");
+?>
+
+<?php include "inc/header.php"; ?>
+
+<div class="recharge-header">
+<a href="index.php"><i class="fa fa-arrow-left"></i></a>
+<span>Select the recharge currency</span>
+</div>
+
+<div class="recharge-container">
+
+<?php while($row = $methods->fetch(PDO::FETCH_ASSOC)): ?>
+
+<a href="deposit.php?id=<?php echo $row['id']; ?>" class="recharge-item">
+
+<div class="recharge-left">
+
+<img src="<?php echo $row['image']; ?>" class="recharge-icon">
+
+<span class="recharge-name">
+<?php echo htmlspecialchars($row['name']); ?>
+</span>
+
+</div>
+
+<div class="recharge-right">
+<i class="fa-solid fa-angle-right"></i>
+</div>
+
+</a>
+
+<?php endwhile; ?>
+
+</div>
+
+<?php include "inc/footer.php"; ?>
